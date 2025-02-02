5aa168e825aedf98b38a25af95e909228a384f7a
bloora <ba997801iop@gmail.com>
2025-02-02 21:10:58 +0300

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 Placeholder for adding interactivity (e.g., cart functionality)
 console.log("Welcome to BALORA Store!");
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+        delay: 3000, // تبديل الصورة كل 3 ثوانٍ
+        disableOnInteraction: false
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
